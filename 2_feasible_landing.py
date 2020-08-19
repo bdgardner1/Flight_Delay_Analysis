672eb2a3b0a76cbae401c98b752c3ee15dae55d8
bdgardner1 <43339667+bdgardner1@users.noreply.github.com>
2020-08-19 13:44:32 +0100

    Annonymised

--- 2_feasible_landing.py
+++ 2_feasible_landing.py
@@ -5,8 +5,8 @@ Created on Wed May  6 11:29:20 2020
 @author: GardneB
 """
 #Checks Monte Carlo simuations to determine whether they conform to feasible landing patters.
-#No more than 8 flights can land in a 15 minute window.
-#No more than 12 flights can land in a 30 minute window. 
+#No more than 8 flights can land in an X minute window.
+#No more than 12 flights can land in a 2X minute window. 
 
 import pandas as pd
 from datetime import timedelta
@@ -15,34 +15,34 @@ from datetime import timedelta
 mc = pd.read_csv("")
 #Convert arrival time to datetime format
 mc['Arrival_Sim'] = pd.to_datetime(mc['Arrival_Sim'])
-
-#Define function to create 15 minute interval period time based on flight arrival time 
-def period_15(row):
+X =  ()#enter number of minutes for time window
+#Define function to create X minute interval period time based on flight arrival time 
+def period_X(row):
     ET = row['Arrival_Sim']
-    interval = ET - timedelta(minutes=ET.minute % 15,
+    interval = ET - timedelta(minutes=ET.minute % X,
                              seconds=ET.second,
                              microseconds=ET.microsecond)
     return interval
 #Define function to create 30 minute interval period time based on flight arrival time 
-def period_30(row):
+def period_2X(row):
     ET = row['Arrival_Sim']
-    interval = ET - timedelta(minutes=ET.minute % 30,
+    interval = ET - timedelta(minutes=ET.minute % X*2,
                              seconds=ET.second,
                              microseconds=ET.microsecond)
     return interval
-#create new column of 15 min interval period times
-mc['period_15'] = mc.apply(period_15,axis=1)
-#create new column of 30 min interval period times
-mc['period_30'] = mc.apply(period_30,axis=1)
-#Groups MC df by scenario number and 15 minute period. Counts the number in each period. 
-mc_15 = mc.groupby(['SC_NO', 'period_15'])['IATA'].count()
-mc_15 = mc_15.reset_index()
-#Groups MC df by scenario number and 30 minute period. Counts the number in each period. 
-mc_30 = mc.groupby(['SC_NO', 'period_30'])['IATA'].count()
-mc_30 = mc_30.reset_index()
-#Creates new dfs listing periods with more than feasible number of landings
-v15 = mc_15.loc[mc_15['IATA']>8]
-v30 = mc_30.loc[mc_30['IATA']>12]
+#create new column of X min interval period times
+mc['period_X'] = mc.apply(period_X,axis=1)
+#create new column of 2X min interval period times
+mc['period_2X'] = mc.apply(period_2X,axis=1)
+#Groups MC df by scenario number and X minute period. Counts the number in each period. 
+mc_X = mc.groupby(['SC_NO', 'period_X'])['IATA'].count()
+mc_X = mc_X.reset_index()
+#Groups MC df by scenario number and 2X minute period. Counts the number in each period. 
+mc_2X = mc.groupby(['SC_NO', 'period_2X'])['IATA'].count()
+mc_2X = mc_2X.reset_index()
+#Cre2es new dfs listing periods with more than feasible number of landings
+v15 = mc_X.loc[mc_X['IATA']>8]
+v30 = mc_2X.loc[mc_2X['IATA']>12]
 
 print(v15, v30)
 
